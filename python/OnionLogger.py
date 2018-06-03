16d526763771ffc2f662bd31d4177811e1e63acc
Foxo <foxltd@protonmail.ch>
2018-06-03 17:40:58 +1200

    Finished tests, working

--- python/OnionLogger.py
+++ python/OnionLogger.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python3
+"""
+OnionLogger for easy inheritance and hiearchical logging with dynamic sorting.
+"""
+__author__ = 'jorxster@gmail.com'
+__date__ = '27 May 2018'
+__version__ = "0.1.0"
+
 import inspect
 import logging
+import os
 import pickle
+import tempfile
 import time
 
 # max at roughly 50MB mem, that's a lot of logs
 # Change to 0 for unlimited
 MAX_LOGS = 49999
 KEEP_UNIQUE_ONLY = False
+VERBOSITY = logging.INFO
 
 
 class Msg(object):
-    SORTING_KEYS = ['level', 'time', 'func_name']
+    SORTING_KEYS = ['level', 'time']
 
     def __init__(self, message_str, level=None) -> None:
         for key in self.SORTING_KEYS:
@@ -18,7 +29,13 @@ class Msg(object):
         self.message = message_str
         self._level = level
         self._time = time.time()
-        self._function = inspect.stack()[2].function
+        self._function = inspect.stack()[3].function
+
+    def __eq__(self, other):
+        return self.message == other.message
+
+    def __ne__(self, other):
+        return self.message != other.message
 
     def __repr__(self) -> str:
         return '<OnionLogger.Msg(time={}, level={}, function={}) at {:x}>' \
@@ -29,10 +46,16 @@ class Logger(object):
 
     def __init__(self):
         self._messages = []
-        self.len = 0
-        self.max_limit = MAX_LOGS
+        self._len = 0
+
+    @property
+    def len(self):
+        return len(self._messages)
 
     # Standard methods
+    def debug(self, msg):
+        self.log(msg, level=logging.DEBUG)
+
     def info(self, msg):
         self.log(msg)
 
@@ -45,20 +68,24 @@ class Logger(object):
     # Base method
     def log(self, msg, level=logging.INFO):
 
+        message = Msg(msg, level=level)
+
+        # print if meets verbosity criteria
+        if level >= VERBOSITY:
+            # TODO : replace this with logging
+            print(msg)
+
         # if discarding duplicate logs
         if KEEP_UNIQUE_ONLY:
             try:
-                self._messages.remove(msg)
+                self._messages.remove(message)
             except ValueError:
                 pass
-
-        message = Msg(msg, level=level)
         self._messages.append(message)
-        self.len += 1
 
         # if limit exceeded, pop first-most item
-        if self.max_limit:
-            if self.len > self.max_limit:
+        if MAX_LOGS:
+            if self.len > MAX_LOGS:
                 self._messages.pop(0)
 
     # sorted return methods
@@ -71,19 +98,27 @@ class Logger(object):
     def return_func_sort(self):
         return sorted(self._messages, key=lambda x: x._function)
 
+    # other methods
+    def reset(self):
+        """
+        Erase all logs
+        """
+        self._messages = []
+        self.info('OnionLogger reset, logs erased')
+
     def serialize(self):
         return pickle.dumps(self)
 
-    def save_to_disk(self, path):
-        with open(path, 'rb') as w:
-            w.write(self.serialize)
+    def save_to_disk(self, path=None):
 
-    # Todo: delete test functions
-    def test(self):
+        if not path:
+            # construct temp / time filepath
+            path = os.path.join(
+                tempfile.gettempdir(),
+                time.strftime('%Y%m%d_%H%M%S.olog')
+            )
 
-        for i in range(0, 10):
-            self.test2()
-            self.log('Privet')
-
-    def test2(self):
-        self.log('Hello World')
+        with open(path, 'wb') as w:
+            self.log('OnionLogger.Logger: serializing and '
+                     'writing to path -- \n\t{}'.format(path))
+            w.write(self.serialize())
--- tests/test_onion_logger.py
+++ tests/test_onion_logger.py
@@ -1,19 +1,67 @@
 #!/usr/bin/env python3
-
-import os
-import sys
+import logging, os, sys, unittest
 
 THIS_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(THIS_PATH + '/python')
 sys.path.append(THIS_PATH + '/python')
 import OnionLogger
 
 
-def main():
-    logger = OnionLogger.Logger()
-    logger.test()
-    print(logger.return_func_sort())
+class TestOnionLogger(unittest.TestCase):
+
+    def setUp(self):
+        self.instance = OnionLogger.Logger()
+
+    def test_serializing(self):
+        self.instance.log('Quoth the raven, \'Nevermore\'')
+        self.instance.save_to_disk()
+        self.instance.reset()
+
+    def test_inspection(self):
+        self.nested()
+        self.assertEqual(
+            self.instance._messages[-1]._function,
+            'nested')
+        self.instance.reset()
+
+    def nested(self):
+        self.instance.warn('\'Tis the wind and nothing more!')
+
+    def test_max_limit(self):
+        """
+        Test the MAX_LOGS constant. This pops items from
+        the beginning of the list of logs keeping the total
+        under the maximum allowed.
+        """
+        OnionLogger.MAX_LOGS = 3
+        for x in range(5):
+            self.instance.log(x, logging.DEBUG)
+
+        self.assertEqual(self.instance.len, 3)
+        self.instance._messages = []
+
+        OnionLogger.MAX_LOGS = 0
+        for x in range(5):
+            self.instance.log(x, logging.DEBUG)
+        self.assertEqual(self.instance.len, 5)
+        self.instance.reset()
+
+    def test_keep_unique_only(self):
+        """
+        Test the KEEP_UNIQUE_ONLY constant. This ensures
+        that duplicates logs are removed keeping only the
+        latest.
+        """
+        self.instance.max_limit = 0
+        OnionLogger.KEEP_UNIQUE_ONLY = True
+        for x in range(10):
+            self.instance.log('Ominous bird of yore.',
+                              logging.DEBUG)
+        self.assertEqual(len(self.instance._messages), 1)
+        self.assertEqual(self.instance.len, 1)
+        self.instance.reset()
+        OnionLogger.KEEP_UNIQUE_ONLY = False
 
 
 if __name__ == '__main__':
-    main()
+    logger = OnionLogger.Logger()
+    print(logger.return_func_sort())
